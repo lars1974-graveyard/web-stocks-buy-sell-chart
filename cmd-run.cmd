6f365b5b384d74cb4528744d06c5835a347f9274
lars1974 <lars.jensen.74@gmail.com>
2020-05-10 13:27:52 +0200

    migrated to snowpack 2.0 (works from git-bash)

--- cmd-run.cmd
+++
@@ -1,2 +0,0 @@
-:: browser-sync must be installed: npm install -g browser-sync
-browser-sync src -f src --index index.html
--- src/views/CounterControl.js
+++ src/views/CounterControl.js
@@ -1,10 +1,17 @@
-import { html } from '../lib/lit-element.js';
+import { html } from 'lit-element';
 import AbstractElement from './AbstractElement.js';
 
 export default class CounterControl extends AbstractElement {
     render() {
         return html`
-        <link rel="stylesheet" type="text/css" href="../css/counter-control.css" />
+        <style>
+        button  {
+          border-radius: 5px;
+          padding: 5;
+          background-color: blueviolet;
+          color: white;
+        }
+        </style>
         <h1>Hmmm</h1>
         <button @click=${this.onCount.bind(this)}>count</button>
         <button @click=${this.onReset.bind(this)}>reset</button>
