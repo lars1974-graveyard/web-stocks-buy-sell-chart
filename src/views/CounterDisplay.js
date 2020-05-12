d4d214b55b20db47e654ab8b99c41d33f17ab0ea
lars1974 <lars.jensen.74@gmail.com>
2020-05-12 22:25:46 +0200

    added chart

--- src/views/CounterDisplay.js
+++ src/views/CounterDisplay.js
@@ -1,10 +1,23 @@
 import { html } from 'lit-element';
 import AbstractElement from './AbstractElement.js';
+import '@google-web-components/google-chart';
+
 
 export default class CounterDisplay extends AbstractElement {
+   
+    
     render() {
         return html`
-        <H1>${this.state.settings.count}</h1>
+        <style>
+        google-chart {
+            height: 50%;
+            width: 50%;
+        }
+        </style>
+        <h1>${this.state.settings.count}</h1>
+        <google-chart data='[["Month", "Days"], ["Jan", 31]]'></google-chart>
+      </google-chart>
+      <h1>after chart</h1>
         `;
     }
 }
